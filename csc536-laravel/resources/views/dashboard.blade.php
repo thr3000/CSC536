8eb9a724115b08e2ed789c16816ff3854cdeb621
priyakaushik1403 <123706042+priyakaushik1403@users.noreply.github.com>
2024-03-11 19:59:11 -0700

    Update dashboard.blade.php

--- csc536-laravel/resources/views/dashboard.blade.php
+++ csc536-laravel/resources/views/dashboard.blade.php
@@ -115,22 +115,3 @@
     </script>
   </body>
 </html>
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-        <title>Dashboard</title>
-
-        <!-- Styles -->
-        <style>
-            
-        </style>
-    </head>
-    <body>
-        <h1>Dashboard</h1>
-    </body>
-    <script>
-    </script>
-</html>
