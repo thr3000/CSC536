b747539389a2d0f5990abfbc312c5c1263d93b8b
david-mclain <davidmclain98@gmail.com>
2024-04-01 19:22:16 -0700

    model stuff i think

--- TestModel/models.py
+++ TestModel/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
+'''
 # Create your models here.
 class Goals(models.Model):
     goalTitle = models.CharField(
@@ -19,3 +21,58 @@ class Account(models.Model):
     username = models.CharField(max_length=20)
     email = models.CharField(max_length=200)
     password = models.CharField(max_length=200)
+'''
+
+class User(models.Model):
+    user_id = models.AutoField(
+        primary_key = True
+    )
+    username = models.CharField(
+        max_length = 16, 
+        validators = [MinLengthValidator(5)], 
+        unique = True, 
+        null = False
+    )
+    email = models.EmailField(
+        unique = True, 
+        null = True
+    )
+    password = models.CharField(
+        max_length = 32
+    )
+
+class Goal(models.Model):
+    goal_id = models.AutoField(
+        primary_key = True
+    )
+    user = models.ForeignKey(
+        User, 
+        on_delete = models.CASCADE
+    )
+    title = models.CharField(
+        max_length = 64
+    )
+    date_created = models.DateTimeField(
+        auto_now_add = True
+    )
+    description = models.CharField(
+        max_length = 256
+    )
+    has_completed = models.BooleanField(
+        default = False
+    )
+    
+class SubGoal(models.Model):
+    subgoal_id = models.AutoField(
+        primary_key = True
+    )
+    goal = models.ForeignKey(
+        Goal, on_delete = models.CASCADE
+    )
+    deadline = models.DateField()
+    description = models.CharField(
+        max_length = 256
+    )
+    has_completed = models.BooleanField(
+        default = False
+    )
--- TestModel/views.py
+++ TestModel/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from TestModel.models import Goals, Account
+from TestModel.models import Goal, User
 from django.contrib import auth
 from django.shortcuts import render, redirect
 import json
@@ -12,7 +12,7 @@ def login(request):
         data = json.loads(request.body)
         email = data.get("email")
         password = data.get("password")
-        user = Account.objects.filter(email = email)
+        user = User.objects.filter(email = email)
         for u in user:
             if u.password == password:
                 return JsonResponse({'message': 'login successfully'}, status=200)
@@ -21,25 +21,27 @@ def login(request):
         return render(request, 'login.html')
 
 def store_goals(data):
-    cnt = len(Goals.objects.all())
+    cnt = len(Goal.objects.all())
     goalTitle = data.get("goalTitle")
     subgoals = data.get("subgoals")
     timelineDate = data.get("timelineDate")
     timelineTime = data.get("timelineTime")
-    goals = Goals(cnt, goalTitle, subgoals, timelineDate, timelineTime)
+    goals = Goal(cnt, goalTitle, subgoals, timelineDate, timelineTime)
     goals.save()
 
 def scan_goals():
-    goals = Goals.objects.all()
+    goals = Goal.objects.all()
     goals_dic = {}
     for g in goals:
         goals_dic[g.id] = [g.goalTitle,g.subgoals,g.timelineDate,g.timelineTime]
     return goals_dic
+    
 def goals(request):
     if request.method == 'GET':
         goals = scan_goals()
         print(goals)
         return render(request,"goals.html", {"goals":goals})
+        
 def dashboard(request):
     if request.method == 'GET':
         return render(request, 'dashboard.html')
@@ -50,14 +52,14 @@ def dashboard(request):
         goals = scan_goals
         return JsonResponse({'message': str(goals)}, status=200)
 
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 def register(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         username = data.get("username")
         email = data.get("email")
         password = data.get("password")
-        new_account = Account(username=username, email=email, password=password)
+        new_account = User(username=username, email=email, password=password)
         new_account.save()
         return JsonResponse({'message': 'register successfully'}, status=200)
     elif request.method == 'GET':
