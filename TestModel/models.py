55998a4a0428c41326d2c8dbde77b9dc92230287
david-mclain <davidmclain98@gmail.com>
2024-04-19 14:50:31 -0700

    idk models attempt not working

--- TestModel/models.py
+++ TestModel/models.py
@@ -1,32 +1,31 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-'''
+
 # Create your models here.
-class Goals(models.Model):
-    goalTitle = models.CharField(
-        max_length=100
-    )
-    subgoals = models.CharField(
-        max_length=100
-    )
-    timelineDate = models.CharField(
-        max_length=100
-    )
-    timelineTime = models.CharField(
-        max_length=100
-    )
+'''
+class Account(models.Model):
+    username = models.CharField(max_length=20, unique=True)
+    email = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=200)
+
+class Goal(models.Model):
+    user=models.ForeignKey(Account, on_delete=models.CASCADE)
+    goalTitle = models.CharField(max_length=100)
+    
+    completed = models.BooleanField(default=False)
+
+class Subgoal(models.Model):
+    goal = models.ForeignKey(Goal, related_name='subgoals', on_delete=models.CASCADE)
+    subgoalTitle = models.CharField(max_length=100)
+    timelineDate = models.CharField(max_length=100)
+    timelineTime = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+'''
+
 
 class Account(models.Model):
-    username = models.CharField(max_length=20)
-    email = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)
-'''
-
-class User(models.Model):
-    user_id = models.AutoField(
-        primary_key = True
-    )
     username = models.CharField(
         max_length = 16, 
         validators = [MinLengthValidator(5)], 
@@ -42,37 +41,44 @@ class User(models.Model):
     )
 
 class Goal(models.Model):
-    goal_id = models.AutoField(
-        primary_key = True
-    )
-    user = models.ForeignKey(
-        User, 
+    CATEGORY_CHOICES = [
+        ('long_term', 'Long Term'),
+        ('short_term', 'Short Term'),
+        ('study', 'For Study'),
+        ('work', 'For Work'),
+    ]
+    STATUS_CHOICES = [
+        ('not_started_yet', 'Not Started Yet'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+    ]
+
+    user = models.ForeignKey(Account, on_delete = models.CASCADE)
+    goalTitle = models.CharField(max_length = 64)
+    description = models.CharField(max_length = 256, null = True)
+    progress = models.IntegerField(validators = [MinValueValidator(0), MaxValueValidator(100)])
+    category = models.CharField(max_length = 20, choices = CATEGORY_CHOICES, default = 'long_term')
+    status = models.CharField(max_length = 32, choices = STATUS_CHOICES)
+    
+class Subgoal(models.Model):
+    goal = models.ForeignKey(
+        Goal, 
+        related_name = 'subgoals',
         on_delete = models.CASCADE
     )
-    title = models.CharField(
-        max_length = 64
+    subgoalTitle = models.CharField(
+        max_length = 100
     )
-    date_created = models.DateTimeField(
-        auto_now_add = True
+    timelineDate = models.CharField(
+        max_length = 100
+    )
+    timelineTime = models.CharField(
+        max_length = 100
     )
     description = models.CharField(
-        max_length = 256
+        max_length = 256,
+        null = True
     )
-    has_completed = models.BooleanField(
-        default = False
-    )
-    
-class SubGoal(models.Model):
-    subgoal_id = models.AutoField(
-        primary_key = True
-    )
-    goal = models.ForeignKey(
-        Goal, on_delete = models.CASCADE
-    )
-    deadline = models.DateField()
-    description = models.CharField(
-        max_length = 256
-    )
-    has_completed = models.BooleanField(
+    completed = models.BooleanField(
         default = False
     )
